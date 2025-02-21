b58c55a20ff4b3c225e6ccf9a44c9c79c386d6d7
viperrau <perrault.victor92@gmail.com>
2025-02-21 16:35:25 +0800

    Added use of params

--- DrawingPackageSimulation/Program.cs
+++ DrawingPackageSimulation/Program.cs
@@ -2,16 +2,13 @@
 
 try
 {
-    List<IWidget> widgets = new List<IWidget>
-        {
-            new Rectangle(10, 10, 30, 40),
-            new Square(15, 30, 35),
-            new Ellipse(100, 150, 300, 200),
-            new Circle(1, 1, 300),
-            new Textbox(5, 5, 200, 100, "sample text")
-        };
+    var drawingSimulation = new DrawingSimulation(
+                                new Rectangle(10, 10, 30, 40),
+                                new Square(15, 30, 35),
+                                new Ellipse(100, 150, 300, 200),
+                                new Circle(1, 1, 300),
+                                new Textbox(5, 5, 200, 100, "sample text"));
 
-    var drawingSimulation = new DrawingSimulation(widgets);
     drawingSimulation.Draw();
 }
 catch (Exception ex)
